a96438f044dd6c546e35681f4599ea00a9072cd3
MaggieNush <maggienungari.mn@gmail.com>
2026-01-12 17:06:34 +0300

    Learning Python

---
+++ freecodecamp/lab.py
@@ -0,0 +1,9 @@
+"""Build a Number Pattern Generator"""                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+def number_pattern(n):
+    if not isinstance(n, int):
+        return 'Argument must be an integer value.'
+    if n < 1:
+        return 'Argument must be an integer greater than 0.'
+    return ' '.join(str(i) for i in range(1, n+1))
+
+print(number_pattern(6))
